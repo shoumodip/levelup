c6f706ada9e1d7ecc02b9a02f9f1492c262926b7
Shoumodip Kar <shoumodipkar@gmail.com>
2024-07-21 09:28:31 +0530

    Bring back rewards

--- index.js
+++ index.js
@@ -85,14 +85,40 @@ class Task {
         this.last = last;
     }
 }
+class Reward {
+    constructor(title, cost) {
+        this.title = title;
+        this.cost = cost;
+    }
+}
 const stats = [];
 const tasks = [];
-let level = 1;
+const rewards = [];
+const profile = {
+    level: 1,
+    points: 0
+};
 const tutorial = {
     stats: false,
     tasks: false,
+    rewards: false,
 };
 function loadData() {
+    {
+        const data = localStorage["levelupStats"];
+        if (data) {
+            const lines = data.split("\n");
+            profile.level = parseInt(lines[0]);
+            for (let i = 1; i < lines.length; i++) {
+                const line = lines[i];
+                if (line === "") {
+                    continue;
+                }
+                const j = line.indexOf(" ");
+                stats.push(new Stat(line.slice(j + 1), parseInt(line.slice(0, j))));
+            }
+        }
+    }
     {
         const data = localStorage["levelupTasks"];
         if (data) {
@@ -105,21 +131,17 @@ function loadData() {
         }
     }
     {
-        const data = localStorage["levelupProfile"];
+        const data = localStorage["levelupRewards"];
         if (data) {
             const lines = data.split("\n");
-            level = parseInt(lines[0]);
-            let start = 1;
-            if (lines[start].indexOf(" ") === -1) {
-                start++;
-            }
-            for (let i = start; i < lines.length; i++) {
+            profile.points = parseInt(lines[0]);
+            for (let i = 1; i < lines.length; i++) {
                 const line = lines[i];
+                if (line === "") {
+                    continue;
+                }
                 const j = line.indexOf(" ");
-                stats.push(new Stat(line.slice(j + 1), parseInt(line.slice(0, j))));
-            }
-            if (start === 2) {
-                localStorage["levelupProfile"] = level + "\n" + stats.map((v) => v.value + " " + v.title).join("\n");
+                rewards.push(new Reward(line.slice(j + 1), parseInt(line.slice(0, j))));
             }
         }
     }
@@ -129,13 +151,15 @@ function loadData() {
             const lines = data.split("\n");
             tutorial.stats = lines[0] === "true";
             tutorial.tasks = lines[1] === "true";
+            tutorial.rewards = lines[2] === "true";
         }
     }
 }
 function saveData() {
+    localStorage["levelupStats"] = profile.level + "\n" + stats.map((v) => v.value + " " + v.title).join("\n");
     localStorage["levelupTasks"] = tasks.map((v) => v.last + " " + v.stat + " " + v.title).join("\n");
-    localStorage["levelupProfile"] = level + "\n" + stats.map((v) => v.value + " " + v.title).join("\n");
-    localStorage["levelupTutorial"] = tutorial.stats + "\n" + tutorial.tasks;
+    localStorage["levelupRewards"] = profile.points + "\n" + rewards.map((v) => v.cost + " " + v.title).join("\n");
+    localStorage["levelupTutorial"] = tutorial.stats + "\n" + tutorial.tasks + "\n" + tutorial.rewards;
 }
 function drawStat(label, value, index) {
     return setClick(setClass(newHorizontal(setClass(newHeader(label, 1), "stretch"), newHeader(value, 1)), "boxed"), () => index !== -1 ? drawStatEditPage(index) : null);
@@ -143,10 +167,22 @@ function drawStat(label, value, index) {
 function drawTask(task, index) {
     return setClass(newHorizontal(setClick(setClass(newVertical(newHeader(task.title, 1), newHeader(stats[task.stat].title, 2)), "stretch"), () => drawTaskEditPage(index)), domMaybe(newButton("Done", () => {
         task.last = todayTime();
+        profile.points++;
         stats[task.stat].value++;
         saveData();
         drawMainPage();
     }, true), todayTime() !== task.last)), "boxed");
+}
+function drawReward(reward, index) {
+    return setClass(newHorizontal(setClick(setClass(newVertical(newHeader(reward.title, 1), newHeader(reward.cost.toString(), 2)), "stretch"), () => drawRewardEditPage(index)), newButton("Buy", () => {
+        if (profile.points < reward.cost) {
+            drawNotifyPage("Not enough points", drawRewardsPage);
+            return;
+        }
+        profile.points -= reward.cost;
+        saveData();
+        drawNotifyPage("Reward: " + reward.title + "!", drawRewardsPage);
+    }, true)), "boxed");
 }
 function drawStatEditPage(index) {
     const title = newInput("Title");
@@ -208,6 +244,32 @@ function drawTaskEditPage(index) {
         drawMainPage();
     }), index !== -1)), title, type));
 }
+function drawRewardEditPage(index) {
+    const title = newInput("Title");
+    const cost = newInput("Cost", "number");
+    if (index !== -1) {
+        cost.value = rewards[index].cost.toString();
+        title.value = rewards[index].title;
+    }
+    document.body.replaceChildren(newPaddedPage(newHorizontal(newButton("Back", drawRewardsPage), newButton("Done", () => {
+        if (title.value !== "" && cost.value !== "") {
+            if (index === -1) {
+                rewards.push(new Reward(title.value, parseInt(cost.value)));
+                tutorial.rewards = true;
+            }
+            else {
+                rewards[index].cost = parseInt(cost.value);
+                rewards[index].title = title.value;
+            }
+            saveData();
+            drawRewardsPage();
+        }
+    }), domMaybe(newButton("Remove", () => {
+        rewards.splice(index, 1);
+        saveData();
+        drawRewardsPage();
+    }), index !== -1)), title, cost));
+}
 function drawInfo(body, type, main, extra) {
     return setClass(newVertical(newHeader("Info", 1), ...body.map((i) => newHeader(i, 2)), setClass(newHorizontal(extra, newButton("Dismiss", () => {
         tutorial[type] = true;
@@ -224,7 +286,7 @@ function drawMainPage() {
         }
     }
     if (levelup) {
-        level++;
+        profile.level++;
         for (const stat of stats) {
             stat.value -= 10;
         }
@@ -232,7 +294,7 @@ function drawMainPage() {
         drawNotifyPage("Leveled up!");
         return;
     }
-    document.body.replaceChildren(newPaddedPage(newHorizontal(newButton("Stats", drawStatsPage), newButton("Add Task", () => drawTaskEditPage(-1))), ...(tutorial.tasks ? tasks.map(drawTask) : [
+    document.body.replaceChildren(newPaddedPage(newHorizontal(newButton("Stats", drawStatsPage), newButton("Rewards", drawRewardsPage), newButton("Add Task", () => drawTaskEditPage(-1))), ...(tutorial.tasks ? tasks.map(drawTask) : [
         drawInfo([
             "Tasks are daily quests you have to complete in order to gain points",
             "Each task has an associated stat which gains 1 point on completion",
@@ -242,7 +304,7 @@ function drawMainPage() {
     ])));
 }
 function drawStatsPage() {
-    document.body.replaceChildren(newPaddedPage(newHorizontal(newButton("Back", drawMainPage), newButton("Add Stat", () => drawStatEditPage(-1))), drawStat("Level", level.toString(), -1), ...(tutorial.stats ? stats.map((v, i) => drawStat(v.title, v.value + "/10", i)) : [
+    document.body.replaceChildren(newPaddedPage(newHorizontal(newButton("Back", drawMainPage), newButton("Add Stat", () => drawStatEditPage(-1))), drawStat("Level", profile.level.toString(), -1), ...(tutorial.stats ? stats.map((v, i) => drawStat(v.title, v.value + "/10", i)) : [
         drawInfo([
             "Stats are fields of interest you can improve in",
             "Each daily task is assigned a specific stat, which adds a stat point upon completion",
@@ -258,6 +320,16 @@ function drawStatsPage() {
             saveData();
             drawStatsPage();
         }, true), stats.length === 0))
+    ])));
+}
+function drawRewardsPage() {
+    document.body.replaceChildren(newPaddedPage(newHorizontal(newButton("Back", drawMainPage), newButton("Add Reward", () => drawRewardEditPage(-1))), drawStat("Points", profile.points.toString(), -1), ...(tutorial.rewards ? rewards.map(drawReward) : [
+        drawInfo([
+            "Completion of tasks grants reward points, which can be used to buy, well, rewards",
+            "Rewards are any pleasurable activity you wish to partake in, like social media, fast food, etc.",
+            "Note that you personally need to maintain the discipline to not do those activies unless bought",
+            "Click on the 'Add Reward' button to add a reward, and set a point price accordingly"
+        ], "rewards", drawRewardsPage)
     ])));
 }
 function drawNotifyPage(message, back = drawMainPage) {
